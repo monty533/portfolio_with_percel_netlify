e71833991f1c91151691c2ec02da63f884bd9cfd
nitin kumar <nitin.kumar@meon.co.in>
2025-04-07 14:21:35 +0530

    vercel

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 // static file access build hai ye
-app.use(express.static(path.join(__dirname, './client/build')))
+// app.use(express.static(path.join(__dirname, './client/build')))
 // app.get('*', function (req, res) {
 //     res.sendFile(path.join(__dirname, './client/build/index.html'))
 // })
